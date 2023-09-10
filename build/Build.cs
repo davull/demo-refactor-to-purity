d7b6367e8254eddfbd679cb232cda9caea598e5f
David Ullrich <david.ullrich@ullrich-software.de>
2023-09-10 10:34:24 +0200

    Publish artifacts

--- build/Build.cs
+++ build/Build.cs
@@ -20,9 +20,16 @@ class Build : NukeBuild
     [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
     readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
 
+    [GitVersion] readonly GitVersion GitVersioning;
+
     [GitRepository] readonly GitRepository Repository;
 
     [Solution] readonly Solution Solution;
+
+    Project[] ProjectsToPublish => Solution
+        .AllProjects
+        .Where(p => p.Name != "_build" && !p.Name.EndsWith(".Test"))
+        .ToArray();
 
     AbsolutePath OutputDirectory => RootDirectory / "output";
 
@@ -30,29 +37,33 @@ class Build : NukeBuild
         .Before(Clean)
         .Executes(() =>
         {
-            Log.Information("Commit             = {Value}", Repository.Commit);
-            Log.Information("Branch             = {Value}", Repository.Branch);
-            Log.Information("Tags               = {Value}", Repository.Tags);
-            Log.Information("main branch        = {Value}", Repository.IsOnMainBranch());
-            Log.Information("main/master branch = {Value}", Repository.IsOnMainOrMasterBranch());
-            Log.Information("Https URL          = {Value}", Repository.HttpsUrl);
-            Log.Information("SSH URL            = {Value}", Repository.SshUrl);
+            Log.Information("Commit                = {Value}", Repository.Commit);
+            Log.Information("Branch                = {Value}", Repository.Branch);
+            Log.Information("Tags                  = {Value}", Repository.Tags);
+            Log.Information("main branch           = {Value}", Repository.IsOnMainBranch());
+            Log.Information("main/master branch    = {Value}", Repository.IsOnMainOrMasterBranch());
+            Log.Information("Https URL             = {Value}", Repository.HttpsUrl);
+            Log.Information("SSH URL               = {Value}", Repository.SshUrl);
 
-            Log.Information("Solution           = {Value}", Solution);
-            Log.Information("Configuration      = {Value}", Configuration);
-            Log.Information("SemVer             = {Value}", GitVersioning.SemVer);
-            Log.Information("FullSemVer         = {Value}", GitVersioning.FullSemVer);
-            Log.Information("MajorMinorPatch    = {Value}", GitVersioning.MajorMinorPatch);
+            Log.Information("Solution              = {Value}", Solution);
+            Log.Information("Configuration         = {Value}", Configuration);
+            Log.Information("SemVer                = {Value}", GitVersioning.SemVer);
+            Log.Information("AssemblySemVer        = {Value}", GitVersioning.AssemblySemVer);
+            Log.Information("AssemblySemFileVer    = {Value}", GitVersioning.AssemblySemFileVer);
+            Log.Information("InformationalVersion  = {Value}", GitVersioning.InformationalVersion);
+            Log.Information("MajorMinorPatch       = {Value}", GitVersioning.MajorMinorPatch);
 
-            Log.Information("BuildId            = {Value}", AzurePipelines?.BuildId);
-            Log.Information("BuildNumber        = {Value}", AzurePipelines?.BuildNumber);
-            Log.Information("AgentWorkFolder    = {Value}", AzurePipelines?.AgentWorkFolder);
+            Log.Information("BuildId               = {Value}", AzurePipelines?.BuildId);
+            Log.Information("BuildNumber           = {Value}", AzurePipelines?.BuildNumber);
+            Log.Information("AgentWorkFolder       = {Value}", AzurePipelines?.AgentWorkFolder);
         });
 
     Target Clean => _ => _
         .Before(Restore)
         .Executes(() =>
         {
+            OutputDirectory.DeleteDirectory();
+
             RootDirectory
                 .GlobDirectories("*/src/*/obj", "*/src/*/bin")
                 .ForEach(d => d.DeleteDirectory());
@@ -66,8 +77,6 @@ class Build : NukeBuild
                 .EnableLockedMode());
         });
 
-    [GitVersion] readonly GitVersion GitVersioning;
-
     Target Compile => _ => _
         .DependsOn(Restore)
         .Executes(() =>
@@ -76,7 +85,9 @@ class Build : NukeBuild
                 .SetProjectFile(Solution)
                 .SetNoRestore(InvokedTargets.Contains(Restore))
                 .SetConfiguration(Configuration)
-                .SetVersion(GitVersioning.FullSemVer));
+                .SetAssemblyVersion(GitVersioning.AssemblySemVer)
+                .SetFileVersion(GitVersioning.AssemblySemFileVer)
+                .SetInformationalVersion(GitVersioning.InformationalVersion));
         });
 
     AbsolutePath TestResultDirectory => OutputDirectory / ".test-results";
@@ -115,6 +126,63 @@ class Build : NukeBuild
                             new string[] { x }));
             }
         });
+
+    AbsolutePath PublishOutputDirectory => OutputDirectory / "publish";
+
+    Target Publish => _ => _
+        .DependsOn(Compile)
+        .Executes(() =>
+        {
+            DotNetPublish(_ => _
+                .SetConfiguration(Configuration)
+                .SetNoRestore(InvokedTargets.Contains(Restore))
+                .SetNoBuild(InvokedTargets.Contains(Compile))
+                .CombineWith(ProjectsToPublish, (_, project) => _
+                    .SetProject(project)
+                    .SetOutput(PublishOutputDirectory / project.Name)));
+        });
+
+    Target Zip => _ => _
+        .DependsOn(Publish)
+        .Executes(() =>
+        {
+            ProjectsToPublish.ForEach(project =>
+            {
+                var publishDirectory = PublishOutputDirectory / project.Name;
+                var archiveFile = OutputDirectory / $"{project.Name}.zip";
+                Log.Information("Creating archive {ArchiveFile}", archiveFile);
+
+                publishDirectory.ZipTo(archiveFile);
+            });
+        });
+
+    Target PublishArtifacts => _ => _
+        .DependsOn(Zip)
+        .OnlyWhenStatic(() => IsServerBuild)
+        .Executes(() =>
+        {
+            ProjectsToPublish.ForEach(project =>
+            {
+                var archiveFile = OutputDirectory / $"{project.Name}.zip";
+                Log.Information("Publish artifact {ArchiveFile}", archiveFile);
+
+                AzurePipelines?.UploadArtifacts("publish", archiveFile.NameWithoutExtension, archiveFile);
+            });
+        });
+
+    protected override void OnBuildInitialized()
+    {
+        Log.Information("\ud83d\ude80 Build process started");
+
+        base.OnBuildInitialized();
+    }
+
+    protected override void OnBuildFinished()
+    {
+        Log.Information("\ud83d\ude80 Build process finished");
+
+        base.OnBuildFinished();
+    }
 
     public static int Main() => Execute<Build>(x => x.Compile);
 }
